37ecac1bac834ece214e63c8a988877cb4360a0c
Dirk Gausmann <dirk.gausmann@sla.de>
2023-05-11 10:02:39 +0200

    Fix

--- android/src/main/java/com/digaus/capacitor/fullscreen/FullscreenPlugin.java
+++ android/src/main/java/com/digaus/capacitor/fullscreen/FullscreenPlugin.java
@@ -105,14 +105,14 @@ public class FullscreenPlugin extends Plugin {
             this.getBridge().getActivity().getWindow().setDecorFitsSystemWindows(false);
             this.getBridge().getActivity().getWindow().setStatusBarColor(0);
             this.getBridge().getActivity().getWindow().setNavigationBarColor(0);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
             this.getBridge().getActivity().getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                             View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
             this.getBridge().getActivity().getWindow().setStatusBarColor(0);
             this.getBridge().getActivity().getWindow().setNavigationBarColor(0);
-        } else if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+        } else if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
             this.getBridge().getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
